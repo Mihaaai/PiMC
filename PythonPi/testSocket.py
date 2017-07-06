d83624478708144f5ca2c4ca76cc2c6db41b476f
Mihaaai <ghidome@gmail.com>
2017-07-06 07:07:09 +0000

    Added test for python connecting to server

--- PythonPi/testSocket.py
+++ PythonPi/testSocket.py
@@ -1,4 +1,13 @@
-#!user/bin/python
+#!/usr/bin/python
 
-from Socket import SocketIO 
+from socketIO_client import SocketIO, ConnectionError
 
+#server = 'localhost'
+server = '192.168.43.182'
+
+try :
+	with SocketIO(server,3000) as socketIO :
+		socketIO.emit('scan')
+		socketIO.wait(seconds=2)
+except ConnectionError as e :
+	print "Connection Error, guys !"
